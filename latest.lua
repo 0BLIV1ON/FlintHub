746380c586fdbae52e100e11e9cfde166743094a
0BLIV1ON <presleyfamilytribute@gmail.com>
2025-04-26 21:08:55 -0500

    Update latest.lua

--- latest.lua
+++ latest.lua
@@ -1,6 +1,6 @@
 local allowedGameIds = {482742811, 123456789, 987654321}
 
-local GITHUB_RAW_URL = "https://raw.githubusercontent.com/0BLIV1ON/FlintHub/refs/heads/main/latest.lua"
+local GITHUB_RAW_URL = "https://raw.githubusercontent.com/0BLIV1ON/FlintHub/refs/heads/main/main.lua"
 
 local function loadLatestScript()
     local success, latestScript = pcall(game.HttpGet, GITHUB_RAW_URL)
